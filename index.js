9d76c172a8f4fbaef24db08d92608b25fa625f2e
TheKingDimvol <the_king_dimvol@mail.ru>
2021-07-08 00:42:08 +0500

    Some

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 let openedCard = null
-let cardsFound = [false, false, false, false]
-
-const cards = document.querySelectorAll('.card')
-
-cards.forEach(card => card.addEventListener('click', cardClicked))
+let cards = []
+let pairsFound = 0
+let timer
 
 function flipCard(image) {
     if (image.style.visibility === 'visible') {
         image.style.visibility = 'hidden'
     } else {
         image.style.visibility = 'visible'
+    }
+}
+
+function checkVictory() {
+    if (pairsFound === 2) {
+        pairsFound = 0
+        clearInterval(timer)
     }
 }
 
@@ -24,23 +29,36 @@ function cardClicked() {
     
     if (openedCard) {
         if (openedCard.firstElementChild.src === image.src) {
-            cardsFound[openedCard.id] = true
-            cardsFound[this.id] = true
             openedCard.removeEventListener('click', cardClicked)
             this.removeEventListener('click', cardClicked)
             openedCard = null
+            pairsFound++
+            checkVictory()
             return
         }
         
+        document.getElementById('field').style.pointerEvents = "none"
         setTimeout(() => {
             flipCard(openedCard.firstElementChild)
             flipCard(image)
             openedCard = null
-        }, 1000);
+            document.getElementById('field').style.pointerEvents = "auto"
+        }, 1000)
         return
     }
     
     openedCard = this
+}
+
+function startTimer() {
+    let time = 0
+    timer = setInterval(() => {
+        time++
+        let minutes = Math.floor(time / 60)
+        let seconds = time % 60
+        document.getElementById('timer')
+            .innerText = `${minutes > 9 ? minutes : '0' + minutes}:${seconds > 9 ? seconds : '0' + seconds}`
+    }, 1000)
 }
 
 function shuffle() {
@@ -50,12 +68,23 @@ function shuffle() {
     })
 }
 
-shuffle()
-
 function restart() {
+    clearInterval(timer)
+    startTimer()
+    openedCard = null
     cards.forEach(card => {
         card.firstElementChild.style.visibility = 'hidden'
         card.addEventListener('click', cardClicked)
     })
     shuffle()
+}
+
+function start() {
+    startTimer()
+    cards = document.querySelectorAll('.card')
+    cards.forEach(card => {
+        card.addEventListener('click', cardClicked)
+    })
+    shuffle()
+    document.getElementById('start').disabled = true
 }
